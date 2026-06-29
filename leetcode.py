e8ca8f12208333cdd7e3d797c5b965a8c7d3d9ec
facundollanos <facundollanos64@gmail.com>
2026-06-28 23:18:49 -0300

    subidos ejercicios 28 y 29

--- leetcode.py
+++ leetcode.py
@@ -21,3 +21,40 @@ class Solution:
                 nums[index] = nums[i]
                 index += 1
         return index
+
+
+#EJERCICIO 28
+class Solution:
+    def strStr(self, haystack, needle):
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+
+
+#EJERCICIO 29
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == divisor:
+            return 1
+        if dividend == -2**31 and divisor == -1:
+            return (2**31) - 1 
+        
+        if divisor == 1:
+            return dividend
+        
+        sign = -1 if (dividend < 0) ^ (divisor < 0) else 1
+        
+        n, d = abs(dividend), abs(divisor)
+        ans = 0
+
+        while n >= d:
+            p = 0
+            while n >= (d << p):
+                p += 1
+            
+            p -= 1
+            n -= (d << p)
+            ans += (1 << p)
+
+        return min(max(sign * ans, -2**31), 2**31 - 1)
