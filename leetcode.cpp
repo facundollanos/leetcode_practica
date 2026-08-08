c100f030417b991a8ee6c6926c5ab68f1c34d9db
facundollanos <facundollanos64@gmail.com>
2026-08-07 23:55:02 -0300

    exercise 232

--- leetcode.cpp
+++ leetcode.cpp
@@ -160,6 +160,42 @@ public:
 };
 
 
+///232. Implement Queue using Stacks
+class MyQueue {
+private:
+    stack<int> input;
+    stack<int> output;    
+
+public:
+    MyQueue() {}
+
+    void push(int x) {
+        input.push(x);
+    }
+
+    int pop() {
+        peek();
+        int val = output.top();
+        output.pop();
+        return val;
+    }
+
+    int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
+
+
 //171. Excel Sheet Column Number
 class Solution {
 public:
